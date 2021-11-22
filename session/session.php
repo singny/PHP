862167910f60f0d2878f5486f3cc43e3aa6c6c36
sieun park <89372116+singny@users.noreply.github.com>
2021-11-22 13:17:21 +0900

    세션 이용하기

--- session/session.php
+++ session/session.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['is_login'])){
 }
 ?>
 <html>
-    <body>
+    <body> 
         <?php echo $_SESSION['nickname'];?>님 환영합니다<br />
         <a href="./logout.php">로그아웃</a>    
     </body>
