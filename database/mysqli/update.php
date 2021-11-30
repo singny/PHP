91b653f597a074ef9268063d1871472264bd7d8c
sieun park <89372116+singny@users.noreply.github.com>
2021-11-30 14:44:20 +0900

    update

---
+++ database/mysqli/update.php
@@ -0,0 +1,44 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', 'tldms4658', 'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn,$sql);
+$list = '';
+while($row = mysqli_fetch_array($result)){
+    $escaped_title = htmlspecialchars($row['title']);   // XSS 방지
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
+}
+
+$article = array(
+    'title'=>'Welcome',
+    'description'=>'Hello, web'
+);
+$update_link = '';
+if(isset($_GET['id'])){
+    $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);   // SQL Injection 방지
+    $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result);
+    $article['title'] = htmlspecialchars($row['title']);    // XSS 방지
+    $article['description'] = htmlspecialchars($row['description']);    // XSS 방지
+    $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="utf-8">
+        <title>WEB</title>
+    </head>
+    <body>
+        <h1><a href="index.php">WEB</a></h1>
+        <ol><?=$list?></ol>
+        <form action="process_update.php" method="POST">
+            <input type="hidden" name="id" value="<?=$_GET['id']?>">
+            <p><input type="text" name="title" placeholder="title" value="<?=$article['title']?>"></p>
+            <p><textarea name="description"
+            placeholder="description"><?=$article['description']?></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+    </body>
+</html>
